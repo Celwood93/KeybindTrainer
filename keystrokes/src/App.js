4251de5b68c8015512f99f37fd88c027505d337a
Cameron Elwood <celwood93@gmail.com>
2018-11-12 08:22:36 -0800

    forgot to save

--- keystrokes/src/App.js
+++ keystrokes/src/App.js
@@ -12,9 +12,9 @@ class App extends Component {
 		//Initial values, I think these should be set from a webpage that occurs before here. None of these db calls should be done in this class tbh
 		//they should be passed from something above (no idea how to do that yet)
 		this.state = {
-			keybindings: {'gettingStarted': {'spell': 'getting started!'}},	
+			keybindings: { gettingStarted: { spell: 'getting started!' } },
 			keys: ['gettingStarted'],
-			key: 'gettingStarted'
+			key: 'gettingStarted',
 		};
 	}
 
@@ -26,54 +26,64 @@ class App extends Component {
 			const snapshot = await ref.child('/Keybindings/1').once('value');
 			const keybindings = snapshot.val();
 			console.log('keybindings from the server', keybindings);
-			const keyOptions =  Object.keys(keybindings).reduce((map, obj) => { 
+			const keyOptions = Object.keys(keybindings).reduce((map, obj) => {
 				map[obj] = keybindings['1'][obj];
-				return map
+				return map;
 			}, {});
 
-
-			this.setState({ 
-				keybindings, 
-				keys: Object.keys(keybindings), 
-				key: "1"
-			}, () => {
-				this.pickRandomElement();
-			});
+			this.setState(
+				{
+					keybindings,
+					keys: Object.keys(keybindings),
+					key: '1',
+				},
+				() => {
+					this.pickRandomElement();
+				}
+			);
 		} catch (error) {
 			console.warn(error);
 		}
 	}
 
-	handleKeyPress = (e) => {
+	handleKeyPress = e => {
 		if (!e.metaKey) {
 			e.preventDefault();
-		} 
-		const keyPressed = {
-			'key': e.code.toLowerCase().replace(/digit|key|left|right/i, ''),
-			'altKey': e.altKey,
-			'ctrlKey': e.ctrlKey,
-			'shiftKey': e.shiftKey
 		}
-		if (keyPressed.key !== 'shift' && keyPressed.key !== 'alt' && keyPressed.key !== 'control') {
+		const keyPressed = {
+			key: e.code.toLowerCase().replace(/digit|key|left|right/i, ''),
+			altKey: e.altKey,
+			ctrlKey: e.ctrlKey,
+			shiftKey: e.shiftKey,
+		};
+		if (
+			keyPressed.key !== 'shift' &&
+			keyPressed.key !== 'alt' &&
+			keyPressed.key !== 'control'
+		) {
 			let expectedKey = this.state.keybindings[this.state.key];
 			//there is an issue here if the key is not a letter or a number, such as ` or ,
-			if (keyPressed.key === expectedKey.key && 
-				((expectedKey.modifier==='CONTROL' && keyPressed.ctrlKey) ||
-				(expectedKey.modifier==='SHIFT' && keyPressed.shiftKey) ||
-				(expectedKey.modifier==='ALT' && keyPressed.altKey) ||
-				(expectedKey.modifier==='NONE' && !keyPressed.ctrlKey && !keyPressed.altKey && !keyPressed.shiftKey))
-				) {
+			if (
+				keyPressed.key === expectedKey.key &&
+				((expectedKey.modifier === 'CONTROL' && keyPressed.ctrlKey) ||
+					(expectedKey.modifier === 'SHIFT' && keyPressed.shiftKey) ||
+					(expectedKey.modifier === 'ALT' && keyPressed.altKey) ||
+					(expectedKey.modifier === 'NONE' &&
+						!keyPressed.ctrlKey &&
+						!keyPressed.altKey &&
+						!keyPressed.shiftKey))
+			) {
 				this.pickRandomElement();
 			}
 		}
-	}
+	};
 
 	pickRandomElement = () => {
 		const randomKey = this.state.keys[
 			Math.floor(Math.random() * this.state.keys.length)
 		];
-		this.setState({ key: randomKey});
-	}
+		this.setState({ key: randomKey });
+	};
 
 	render() {
 		return (
