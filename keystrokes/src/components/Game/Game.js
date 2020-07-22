0fdb6ce480a36bc1ca0d7590ad8b332cda58d3db
Cameron Elwood <celwood93@gmail.com>
2020-07-22 14:07:36 -0700

    removed unused imports

--- keystrokes/src/components/Game/Game.js
+++ keystrokes/src/components/Game/Game.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, Component, Fragment } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import '../../stylesheets/App.css';
 
